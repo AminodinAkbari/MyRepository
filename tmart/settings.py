7128f9dd0c8564c25c4195538a2cac45f6a0ccdb
Amin Akbari <13amin80badboy@gmail.com>
2022-03-05 18:05:52 +0330

    settings safe

--- tmart/settings.py
+++ tmart/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-edwreh7q^$w3ppk%zndw^8f863q87$wbqs_mik+)!)v0!!yhup'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
