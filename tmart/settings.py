88a82d52171b7398606e38d32be47210bd7d8eb3
Amin Akbari <13amin80badboy@gmail.com>
2022-03-13 16:25:41 +0330

    Almost Ready

--- tmart/settings.py
+++ tmart/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'tmart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': BASE_DIR / config('DB_NAME'),
     }
 }
 
